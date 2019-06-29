68c9d6a42de894a781a777555815b9696f9ae6b2
Ben Wetherfield <bwetherfield@gmail.com>
2019-06-29 15:44:37 -0700

    Remove unneeded

--- Package.swift
+++ Package.swift
@@ -5,33 +5,14 @@ import PackageDescription
 let package = Package(
     name: "Graphics",
     products: [
-//        .library(name: "Geometry", targets: ["Geometry"]),
         .library(name: "Path", targets: ["Path"]),
-//        .library(name: "Rendering", targets: ["Rendering"]),
-//        .library(name: "SVG", targets: ["SVG"]),
-//        .library(name: "QuartzAdapter", targets: ["QuartzAdapter"]),
-//        .library(name: "GraphicsTesting", targets: ["GraphicsTesting"])
     ],
     dependencies: [
-//        .package(url: "https://github.com/drmohundro/SWXMLHash", from: "4.1.1"),
-//        .package(url: "https://github.com/dn-m/Math", from: "0.7.0"),
         .package(url: "https://github.com/bwetherfield/Structure", .branch("minimal-break"))
     ],
     targets: [
 
         // Sources
-//        .target(name: "Geometry", dependencies: ["Math", "DataStructures"]),
         .target(name: "Path", dependencies: ["DataStructures"]),
-//        .target(name: "Rendering", dependencies: ["Path"]),
-//        .target(name: "SVG", dependencies: ["Rendering", "SWXMLHash"]),
-//        .target(name: "QuartzAdapter", dependencies: ["Rendering"]),
-//        .target(name: "GraphicsTesting", dependencies: ["QuartzAdapter"]),
-
-        // Tests
-//        .testTarget(name: "GeometryTests", dependencies: ["Geometry"]),
-        /* .testTarget(name: "PathTests", dependencies: []), */
-//        .testTarget(name: "RenderingTests", dependencies: ["Rendering", "GraphicsTesting"]),
-//        .testTarget(name: "SVGTests", dependencies: ["SVG", "GraphicsTesting"]),
-//        .testTarget(name: "QuartzAdapterTests", dependencies: ["QuartzAdapter", "GraphicsTesting"])
     ]
 )
--- Sources/Path/Path.swift
+++ Sources/Path/Path.swift
@@ -12,84 +12,18 @@ import DataStructures
 
 public struct Path {
     
-    // MARK: - Instance Properties
-//
-//    public var isShape: Bool {
-//        return curves.allSatisfy { curve in curve.order == .linear }
-//    }
-//
-//    /// - Returns: `true` if there are no non-`.close` elements contained herein. Otherwise,
-//    /// `false`.
-//    public var isEmpty: Bool {
-//        return curves.isEmpty
-//    }
-    
-    /// - Returns: The axis-aligned bounding box for `Path`.
-    ///
-    /// - Warning: This uses a simplification technique for calculateing the bounding boxes of
-    /// quadratic and cubic Bézier curves, which may result in some inaccuracy for whacky curves.
-//    ///
-//    public var axisAlignedBoundingBox: Rectangle {
-//        return curves.map { $0.axisAlignedBoundingBox }.nonEmptySum ?? .zero
-//    }
-    
     public let curves: [Int]
     
     // MARK: - Initializers
 
     /// Create a `Path` with a single `curve`.
-    public init(_ curve: Int) {
-        self.init([curve])
-    }
-    
-    /// Create a `Path` with the given `curves`.
-    public init(_ curves: [Int]) {
-        self.curves = curves
-    }
-    
-    /// Create a `Path` with the given `pathElements`.
-//    public init(pathElements: [PathElement]) {
-//
-//        guard
-//            let (head, tail) = pathElements.destructured, case let .move(start) = head
-//        else {
-//            self = Path([])
-//            return
-//        }
-//
-//        let builder = Path.builder.move(to: start)
-//        var last = start
-//
-//        for element in tail {
-//            switch element {
-//            case .move(let point):
-//                builder.move(to: point)
-//                last = point
-//            case .line(let point):
-//                _ = point == last ? builder.close() : builder.addLine(to: point)
-//            case .quadCurve(let point, let control):
-//                builder.addQuadCurve(to: point, control: control)
-//            case .curve(let point, let control1, let control2):
-//                builder.addCurve(to: point, control1: control1, control2: control2)
-//            case .close:
-//                builder.close()
-//            }
-//        }
-//
-//        self = builder.build()
+//    public init(_ curve: Int) {
+//        self.init([curve])
 //    }
-
-    // MARK: - Instance Methods
-//
-//    /// - Returns: Polygonal representation of the `Path`.
-//    public func simplified(segmentCount: Int) -> Polygon {
-//        let vertices = curves.map { $0.simplified(segmentCount: segmentCount) }
-//        let (most, last) = vertices.split(at: vertices.count - 1)!
-//        let merged = most.flatMap { $0.dropLast() } + last.first!
-//        if merged.count == 2 {
-//            return Polygon(vertices: merged + [merged.first!])
-//        }
-//        return Polygon(vertices: merged)
+//    
+//    /// Create a `Path` with the given `curves`.
+//    public init(_ curves: [Int]) {
+//        self.curves = curves
 //    }
 }
 
@@ -99,30 +33,3 @@ extension Path: CollectionWrapping {
         return curves
     }
 }
-
-//extension Path: Collection { }
-
-//extension Path: Additive {
-//
-//    /// Empty path.
-//    public static let zero = Path([])
-//
-//    /// - Returns: New `Path` with elements of two paths.
-//    public static func + (lhs: Path, rhs: Path) -> Path {
-//        return Path(lhs.curves + rhs.curves)
-//    }
-//}
-
-//extension Path: Equatable { }
-
-//extension Path: CustomStringConvertible {
-//
-//    // MARK: - CustomStringConvertible
-//
-//    /// Printed description.
-//    public var description: String {
-//        var result = "Path:\n"
-//        result += curves.map { "  - \($0)" }.joined(separator: "\n")
-//        return result
-//    }
-//}
